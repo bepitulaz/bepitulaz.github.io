8806904b77dac7d1df659cd126ebe65eefef68e7
Asep Bagja Priandana <bepitulaz@gmail.com>
2023-06-04 17:23:58 +0300

    Complete basic home page

--- src/content/config.js
+++ src/content/config.js
@@ -15,6 +15,19 @@ const musicCollection = defineCollection({
   }),
 });
 
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    images: z.array(z.string()),
+    categories: z.array(z.string()),
+    featured: z.boolean().optional(),
+    summary: z.string(),
+  }),
+});
+
 export const collections = {
   music: musicCollection,
+  blog: blogCollection,
 };
