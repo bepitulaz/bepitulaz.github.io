c5292ee372f9522a9682cfe6edc9bba1e7f82f12
Asep Bagja Priandana <bepitulaz@gmail.com>
2023-06-11 11:52:12 +0300

    Change the base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bepitulaz.github.io",
+  site: "https://www.asepbagja.com",
   integrations: [mdx()],
   experimental: {
     assets: true,
